97dac80d5b17d97736c5758e67ffe6196c51490e
Syed Umair Ali <syedumairali.617@gmail.com>
2025-08-03 00:06:30 +0500

    Landing page completed

---
+++ components/logo.tsx
@@ -0,0 +1,11 @@
+const Logo = () => {
+  return (
+    <>
+      <h1 className="font-bold font-5xl text-foreground text-leading tracking-wide">
+        Cognito
+      </h1>
+    </>
+  );
+};
+
+export default Logo;
